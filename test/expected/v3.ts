41db811992cd9dbf6780e3e97c286f3aab3eab05
creeperyang <yangcreeper@hotmail.com>
2017-12-07 21:55:47 +0800

    [Test] add test for main functions

---
+++ test/expected/v3.ts
@@ -0,0 +1,51 @@
+export default {
+    version: {
+        major: 2,
+        minor: 3,
+        revision: 0,
+        flags: { unsync: false, xheader: false, experimental: false },
+    },
+    title: 'Bye Bye',
+    artist: 'Mariah Carey',
+    album: 'E=Mc²',
+    'user-defined-text-information': [{ description: 'Tagging time', value: '2012-07-17T05:28:12' }],
+    year: '2008-04-14',
+    image: {
+        type: 'cover-front',
+        mime: 'image/jpeg',
+        description: 'e',
+    },
+};
+
+// module.exports = {
+//     version: {
+//         v2: {
+//             major: 2,
+//             minor: 3,
+//             revision: 0,
+//             flags: {
+//                 unsync: 0,
+//                 xheader: 0,
+//                 experimental: 0
+//             }
+//         },
+//         v1: {
+//             major: 1,
+//             minor: 1
+//         }
+//     },
+//     title: 'Bye Bye',
+//     artist: 'Mariah Carey',
+//     album: 'E=Mc²',
+//     'user-defined-text-information': 'Tagging time',
+//     year: '2008-04-14',
+//     image: {
+//         type: 'cover-front',
+//         mime: 'image/jpeg',
+//         imageType: null,
+//         description: 'e',
+//     },
+//     comment: '',
+//     track: 0,
+//     genre: ''
+// };
